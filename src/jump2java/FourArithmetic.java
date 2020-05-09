89af27be059c125fe581b6e1732586850fbcb69d
Candice Han <jeeyoung.han82@gmail.com>
2020-05-09 19:41:13 +0200

    first commit from /Users/candicehan/eclipse-workspace/jump2java

---
+++ src/jump2java/FourArithmetic.java
@@ -0,0 +1,14 @@
+package jump2java;
+
+public class FourArithmetic {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(7%3);
+		System.out.println(3%7);
+	}
+}
---
+++ src/jump2java/practice.java
@@ -0,0 +1,12 @@
+package jump2java;
+
+public class practice {
+	public static void main(String[] args) {
+		String a = "hello";
+	    String b = "java";
+	    String c = "hello";
+	    
+	    System.out.println(a.equals(b));
+	    System.out.println(a.equals(c));
+	}
+}
