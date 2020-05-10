007f8c370449499d9b90c7fc8fdf2512e2ea4ec4
Candice Han <jeeyoung.han82@gmail.com>
2020-05-10 17:41:33 +0200

    data type - Array/List/Generics/Map

--- src/jump2java/practice.java
+++ src/jump2java/practice.java
@@ -1,12 +1,22 @@
 package jump2java;
 
+import java.util.ArrayList;
+
 public class practice {
 	public static void main(String[] args) {
-		String a = "hello";
-	    String b = "java";
-	    String c = "hello";
-	    
-	    System.out.println(a.equals(b));
-	    System.out.println(a.equals(c));
+		
+		ArrayList<String> pitches = new ArrayList<String>();
+		
+		pitches.add("138");
+		pitches.add("129");
+		pitches.add("142");
+		
+		System.out.println(pitches.get(1));
+		System.out.println(pitches.size());
+		System.out.println(pitches.contains("142"));
+		System.out.println(pitches.remove("129"));
+		System.out.println(pitches.size());
+		System.out.println(pitches.remove(0));
+		
 	}
 }
