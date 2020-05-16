a0ccbb866e0d66d100914cc031a3ac3fdf04b75a
Candice Han <jeeyoung.han82@gmail.com>
2020-05-16 19:01:57 +0200

    foreach practice

--- src/jump2java/practice.java
+++ src/jump2java/practice.java
@@ -1,13 +1,16 @@
 package jump2java;
 
+import java.util.ArrayList;
+
 public class practice {
 	public static void main(String[] args) {
-		int[] marks = {90, 25, 67, 45, 80};
-		for(int i=0; i<marks.length; i++) {
-		    if (marks[i] < 60) {
-		    	continue;
-		    }
-		    System.out.println((i+1)+"번 학생 축하합니다. 합격입니다.");
-		}
-    }
+	    ArrayList<String> numbers = new ArrayList<String>();
+	    numbers.add("one");
+	    numbers.add("two");
+	    numbers.add("three");
+	    
+	    for(String number: numbers) {
+	    	System.out.println(number);
+	    }
+	}
 }
