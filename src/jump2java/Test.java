65aecb9bf63d6972863791242179c1025e4e5276
Candice Han <jeeyoung.han82@gmail.com>
2020-05-17 22:24:36 +0200

    method practice

---
+++ src/jump2java/Test.java
@@ -0,0 +1,17 @@
+package jump2java;
+
+public class Test {
+	public int sum(int a, int b) {
+	    return a+b;
+	}
+	
+	public static void main(String[] args) {
+	    int a = 3;
+	    int b = 4;
+	    
+	    Test myTest = new Test();
+	    int c = myTest.sum(a, b);
+	    
+	    System.out.println(c);
+	}
+}
