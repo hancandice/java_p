aaec83a14a387f4153ca4307c0000242df2b834b
Candice Han <jeeyoung.han82@gmail.com>
2020-05-18 22:52:00 +0200

    instance variable_test

--- src/jump2java/Test.java
+++ src/jump2java/Test.java
@@ -1,17 +1,14 @@
 package jump2java;
 
 public class Test {
-	public int sum(int a, int b) {
-	    return a+b;
+	public void vartest (int x) {
+		x++;
 	}
 	
-	public static void main(String[] args) {
-	    int a = 3;
-	    int b = 4;
-	    
-	    Test myTest = new Test();
-	    int c = myTest.sum(a, b);
-	    
-	    System.out.println(c);
+	public static void main (String[] args) {
+		int a = 1;
+		Test myTest = new Test();
+		myTest.vartest(a);
+		System.out.println(a);
 	}
 }
