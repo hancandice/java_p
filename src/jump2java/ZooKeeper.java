249302250b923d3c6a7134066251eccdbc6f0d15
Candice Han <jeeyoung.han82@gmail.com>
2020-05-21 19:46:30 +0200

    method_priority

--- src/jump2java/ZooKeeper.java
+++ src/jump2java/ZooKeeper.java
@@ -3,8 +3,17 @@ package jump2java;
 public class ZooKeeper {
 	
 	public void feed(Predator predator) {
+	    System.out.println("feed predator");
 	    System.out.println("feed "+predator.getFood());	
 	}
+	
+	/* public void feed(Tiger tiger) {
+		System.out.println("feed tiger");
+	}
+	
+	public void feed(Lion lion) {
+		System.out.println("feed lion"); 
+	} */
 	
 	public static void main(String[] args) {
 		ZooKeeper zooKeeper = new ZooKeeper();
